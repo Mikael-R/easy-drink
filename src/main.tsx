d340bc03b3c7e4c315ce02fcd38cf8534b297212
mikael-r <mikarg9@gmail.com>
2022-03-11 23:23:16 -0300

    refactor: 💡 create assets folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
-import './global.css'
+import './assets/styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
